308d6006a5f98a30138b0d2e9b78db25f10be41f
Medeiros Neto <ajmedeirosnt@gmail.com>
2022-05-27 15:03:45 -0300

    Criação da Interface IPersonService

---
+++ 02_RestWithASP-NETU5demy_ViaVisualStudio/RestWithASPNETU5demy/RestWithASPNETU5demy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETU5demy.Models;
+using System.Collections.Generic;
+
+namespace RestWithASPNETU5demy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
