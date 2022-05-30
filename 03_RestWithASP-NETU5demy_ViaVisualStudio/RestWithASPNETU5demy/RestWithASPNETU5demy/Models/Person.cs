cd3c052a1050b45534d988a10dc26ccdfd550fd1
Medeiros Neto <ajmedeirosnt@gmail.com>
2022-05-30 14:58:20 -0300

    Criação da conexão e utilização dos Verbos.

---
+++ 03_RestWithASP-NETU5demy_ViaVisualStudio/RestWithASPNETU5demy/RestWithASPNETU5demy/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RestWithASPNETU5demy.Models
+{
+    [Table("PERSON")]
+    public class Person
+    {
+        [Column("PRS_ID")]
+        public long Id { get; set; }
+        [Column("PRS_FIRST_NAME")]
+        public string FirstName { get; set; }
+        [Column("PRS_LAST_NAME")]
+        public string LastName { get; set; }
+        [Column("PRS_ADDRESS")]
+        public string Address { get; set; }
+        [Column("PRS_GENDER")]
+        public string Gender { get; set; }
+    }
+}
