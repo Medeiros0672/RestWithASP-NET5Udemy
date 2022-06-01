8d021abfca4a53453fe92461a26fca1f3aeeb7a7
Medeiros Neto <ajmedeirosnt@gmail.com>
2022-06-01 08:07:59 -0300

    Renomeando a pasta Service para Business e criando a pasta Repository

---
+++ 04_RestWithASP-NETU5demy_ViaVisualStudio/RestWithASPNETU5demy/RestWithASPNETU5demy/Models/Context/OracleContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace RestWithASPNETU5demy.Models.Context
+{
+    public class OracleContext : DbContext
+    {
+        public OracleContext()
+        {
+        }
+
+        public OracleContext(DbContextOptions<OracleContext> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
